cf440a3f57eb5fc5e3104c335ce50990992ee75e
Sean <36542623+Ihadcode@users.noreply.github.com>
2022-11-12 14:44:05 -0500

    Update personal-dictionary.js

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,15 @@
-//Fill in this empty file
+//Emptyarray of words
+let words = [];
+
+
+// settng paramter and returns true if it is found, false
+function search(word){
+    return words.includes(word);
+}
+//addWord function and pushes word added
+function addWord(word){
+    words.push(word)
+
+}
+//declaration is used to export values from search and addWord module
+export{search, addWord};
